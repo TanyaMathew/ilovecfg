3043d4f1b05bdd73326c9b98952b5d7a79d7a97a
Tanya Mathew <tanya.r.mathew@gmail.com>
2014-11-22 14:23:49 +0000

    the bones of our web app

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
 
-get'/' do
-	"Hello world! I lovee cfg"
+get '/' do
+	erb :entrypage
 end
+
+post '/' do
+	@name = params[:user_name]
+
+  if @name.length >= 2
+  	erb :index
+
+  else
+  	erb :apologies
+  end
+end
+
+get '/charitychooser' do
+	erb :charitychooser
+end
+
+
