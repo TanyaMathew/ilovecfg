a5da03ab8219393e8a1b469f566ff0a3f93ac822
Tanya Mathew <tanya.r.mathew@gmail.com>
2014-11-23 00:41:07 +0000

    Changed loads, added the charity chooser, email page, and attempted some of the ruby for the charity chooser but failed, Also lots of CSS pages which might be messy and redundant cause I was messing around a lot with them

--- app.rb
+++ app.rb
@@ -19,4 +19,55 @@ get '/charitychooser' do
 	erb :charitychooser
 end
 
+post '/charitychooser' do
+
+ if params[:typeoforganisation] == "Grassroots" && params[:thecause] == "Healthcare"
+ 	erb :grassroots_healthcare
+
+ elsif params[:typeoforganisation] == "Grassroots" && params[:thecause] == "Education"
+ 	erb :grassroots_education
+ 	
+ elsif params[:typeoforganisation] == "Grassroots" && params[:thecause] == "Environment"
+ 	erb :grassroots_environment
+
+ elsif params[:typeoforganisation] == "Grassroots" && params[:thecause] == "Wildlife"
+ 	erb :grassroots_wildlife
+
+ elsif params[:typeoforganisation] == "International" && params[:thecause] == "Healthcare"
+ 	erb :international_healthcare
+
+ elsif params[:typeoforganisation] == "International" && params[:thecause] == "Education"
+ 	erb :international_education
+
+ elsif params[:typeoforganisation] == "International" && params[:thecause] == "Environment"
+ 	erb :international_environment
+
+ elsif params[:typeoforganisation] == "International" && params[:thecause] == "Wildlife"
+ 	erb :international_wildlife
+
+ elsif params[:typeoforganisation] == "Social Enterprise" && params[:thecause] == "Healthcare"
+ 	erb :enterprise_healthcare
+
+ elsif params[:typeoforganisation] == "Social Enterprise" && params[:thecause] == "Education"
+ 	erb :enterprise_education
+
+ elsif params[:typeoforganisation] == "Social Enterprise" && params[:thecause] == "Environment"
+ 	erb :enterprise_environment
+
+ elsif params[:typeoforganisation] == "Social Enterprise" && params[:thecause] == "Wildlife"
+ 	erb :enterprise_wildlife 	 	 	
+ 
+ else 
+ 	print "You have not chosen any options"
+
+ end
+end
+
+get '/email' do
+	erb :email
+end
+
+## post '/email' do
+## ??????
+## end
 
